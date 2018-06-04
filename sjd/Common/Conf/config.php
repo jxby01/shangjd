76841f2fba2de977e4dac9900cac6ee1593664b5
jxby_zxy <719419739@qq.com>
2018-06-04 09:15:48 +0800

    915
    数据库地址变更

--- sjd/Common/Conf/config.php
+++ sjd/Common/Conf/config.php
@@ -2,10 +2,10 @@
 return array(
 	//'配置项'=>'配置值'
 	'DB_TYPE'   => 'mysql', // 数据库类型
-	'DB_HOST'   => 'localhost', // 服务器地址
+	'DB_HOST'   => '192.168.0.113', // 服务器地址
 	'DB_NAME'   => 'sjd', // 数据库名
-	'DB_USER'   => 'root', // 用户名
-	'DB_PWD'    => 'root', // 密码
+	'DB_USER'   => 'admin', // 用户名
+	'DB_PWD'    => 'admin', // 密码
 	'DB_PORT'   => 3306, // 端口
 	'DB_PARAMS' =>  array(), // 数据库连接参数
 	'DB_PREFIX' => 'sj_', // 数据库表前缀 
